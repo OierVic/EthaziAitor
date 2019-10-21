fae6673287ec69b6ba9e152b5407f6b41daba129
Diego <admin1@HZ327421.H015112.NET>
2019-10-21 12:07:03 +0200

    e

--- EthaziAitor/src/eredua/prueba.java
+++ EthaziAitor/src/eredua/prueba.java
@@ -6,6 +6,6 @@ public class prueba {
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		 DepEmpleKudeatu.IrakurriDepartamentua();
+		 Departamentua.IrakurriDepartamentua();
 	}
 }
