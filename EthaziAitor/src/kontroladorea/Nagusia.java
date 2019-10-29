73f87d756c4cbe66c48942aa99eccbe32b44e06b
Oier Vicente <oier.vicentefi@elorrieta-errekamari.com>
2019-10-29 13:59:48 +0100

    Kontroladorea leihoekin lotu

--- EthaziAitor/src/kontroladorea/Nagusia.java
+++ EthaziAitor/src/kontroladorea/Nagusia.java
@@ -4,6 +4,7 @@ import eredua.Enplegatua;
 import eredua.KonexioaMariaDB;
 import eredua.konexioa;
 import leihoak.OngiEtorria;
+import leihoak.FitxategiaAukeratu;
 import leihoak.Kudeaketa;
 
 public class Nagusia {
@@ -11,14 +12,24 @@ public class Nagusia {
 	public static void main(String[] args) {
 		
 		konexioa konexioa = new konexioa("ethazi");
-		//KonexioaMariaDB konexioamariaDB = new KonexioaMariaDB();
-		//konexioamariaDB.konektatu();
 		
-		OngiEtorria ongEtor = new OngiEtorria();
-		ongEtor.setVisible(true);	
+		//Ventanas
+		Kontroladorea kontroladorea = new Kontroladorea();
+		OngiEtorria ongietorria = new OngiEtorria();
+		FitxategiaAukeratu fitxategiaukeratu = new FitxategiaAukeratu();
+		
+//***********************************************************************************
+		kontroladorea.ateraOngietorria(ongietorria);
+		ongietorria.Kontroladorea(kontroladorea);
+//***********************************************************************************
+		kontroladorea.ateraFitxategiaAukeratu(fitxategiaukeratu);
+		fitxategiaukeratu.Kontroladorea(kontroladorea);
+		
+//***********************************************************************************
 
-		Kudeaketa kudeaketa = new Kudeaketa();
-		kudeaketa.setVisible(true);
+		
+		ongietorria.setVisible(true);
+		
 //		ArrayList<Enplegatua> arrayEnplegatu = new ArrayList<Enplegatua>();
 //		arrayEnplegatu = Enplegatua.CSVEnplegatuakIrakurri();
 //		Enplegatua.EnplegatuakIgo(arrayEnplegatu);
