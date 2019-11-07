2938631e791590c332e7992f93426237e8c52273
diego <diego.gomezur@elorrieta-errekamari.com>
2019-11-07 09:52:47 +0100

    Log prueba funciona

--- EthaziAitor/src/Diego/prueba.java
+++ EthaziAitor/src/Diego/prueba.java
@@ -1,21 +1,33 @@
-   package Diego;
+package Diego;
 
-import java.util.ArrayList;
+import java.util.logging.LogManager;
 
-import eredua.DepKonexio;
-import eredua.Departamentua;
-import eredua.Enplegatua;
-import eredua.konexioa;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
-public class prueba {
 
-	public static void main(String[] args) {
-		Departamentua dep = new Departamentua(1, "hl", "hola");
-	ArrayList<Departamentua> depArrayList = null;
-	ArrayList<Enplegatua> enpArrayList = null;
-	String json = null;
-	ReadDepartJSON.read(json, depArrayList);
-	ReadEnpleJSON.read(json, enpArrayList);
-	}
+public class prueba { 
+final static Logger logger = Logger.getLogger(prueba.class);  
 
+//main method 
+public static void main(String[] args) {  
+	
+	
+  PropertyConfigurator.configure("C:\\Users\\admin1\\git\\EthaziAitor2\\EthaziAitor\\src\\log4j.properties");   
+  if(logger.isInfoEnabled()) logger.info("Application starting...");  
+  System.out.println("Application starting processes here.");   
+  if(logger.isInfoEnabled()) logger.info("Application started successfully");  
+  try{   
+    System.out.println("Application do more stuff here.");   
+    if(logger.isDebugEnabled()) logger.debug(("Application did something"));  
+  } catch (Exception e){   
+    logger.error("Something went wrong :"+e);  
+  }  
+  if(logger.isInfoEnabled()) logger.info("Application finishing up...");  
+  System.out.println("Application end processes here.");  
+  
+  pruebaLog.hola();
+  
+  if(logger.isInfoEnabled()) logger.info("Application finished...");  
+ }
 }
